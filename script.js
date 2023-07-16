3b83b33dfccda1aa0c45bf0601c85c4495c55186
l7aj <mohamed.agday.m@icloud.com>
2023-07-16 09:54:57 +0100

    update the header and make it responsiv

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
- 
+let menubtn = document.getElementById('menu') 
+let navbar = document.getElementById('navbar') 
+let closbtn = document.getElementById('closbtn') 
+
+
+
+menubtn.addEventListener('click', _ => {
+    navbar.classList.toggle('show')
+
+})
+closbtn.addEventListener('click', _ => {
+    navbar.classList.toggle('show')
+
+})
