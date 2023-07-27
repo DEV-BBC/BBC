8fc9fc43cc65d07d7cd3ac3ee19c41ba17802c23
Nouri-Mouad <mouad.nouri2003@gmail.com>
2023-07-27 23:19:05 +0100

    added animation

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ closbtn.addEventListener('click', _ => {
     navbar.classList.toggle('show')
 
 })
+
+const observer = new IntersectionObserver ((entries) => {
+    entries.forEach((entry) =>{
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
